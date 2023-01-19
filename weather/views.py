fc1f68275bad0ca907191e393ce726fffaba2067
DanielVillar <villar_daniel@hotmail.com>
2023-01-19 15:13:14 -0500

    new

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .form import CityForm
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=c6653f6c9abb06e0a3dd17d64a2e8145'
 
     cities = City.objects.all()  # return all the cities in the database
+    if request.method == 'POST':  # only true if form is submitted
+        form = CityForm(request.POST)  # add actual request data to form for processing
+        form.save()  # will validate and save if validate
+    form = CityForm()
     weather_data = []
 
     for city in cities:
@@ -19,5 +24,5 @@ def index(request):
         }
         weather_data.append(weather)  # add the data for the current city into our list
 
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form' : form}
     return render(request, 'index.html', context)  # returns the index.html template
